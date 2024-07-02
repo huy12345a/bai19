c2f9282dd5242a474aa8c480366d2ae92f3d4165
huy12345a <Quanghuy.hmtb@gmail.com>
2024-07-02 13:50:26 +0700

    Email

---
+++ src/Use.java
@@ -0,0 +1,14 @@
+public class Use {
+    /*email: x@gmail.com
+    - chu ,so : [a-zA-Z0-9]
+    - @gmail.com
+    pattern: [a-zA-Z0-9]@gmail.com
+    viet pattern kiemtra dau vao co phai 1 domain hop le vn:
+    phan 1 : (http|https)
+    phan 2 : ://
+    phan 3 :
+     */
+    public static void main(String[] args) {
+        System.out.println("Java Java Java".replaceAll("v\\w", "wi"));
+    }
+}
